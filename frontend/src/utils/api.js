20360b39ac8a623a564947e9486dfd32ee0958b7
hisabuser <harshgoy877@gmail.com>
2026-05-20 18:36:21 +0000

    auto-commit for 51c87a37-b883-407e-9405-0665f6542240

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE = process.env.REACT_APP_BACKEND_URL + "/api";
+const BASE = (process.env.REACT_APP_BACKEND_URL || "") + "/api";
 
 const api = axios.create({ baseURL: BASE });
 
